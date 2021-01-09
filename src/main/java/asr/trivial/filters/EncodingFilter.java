6472ee225fd6539d3736e61669d725ff2fb0196f
Rocky <rockygitt@gmail.com>
2021-01-09 23:46:24 +0100

    New package filters

--- src/main/java/asr/trivial/filters/EncodingFilter.java
+++ src/main/java/asr/trivial/filters/EncodingFilter.java
@@ -1,4 +1,4 @@
-package asr.trivial.servlets;
+package asr.trivial.filters;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -7,21 +7,28 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import javax.servlet.annotation.WebFilter;
-
 import java.io.IOException;
 
-@WebFilter(urlPatterns="/*")
 public class EncodingFilter implements Filter {
 
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+
     request.setCharacterEncoding("utf-8");
     response.setCharacterEncoding("utf-8");
+
     chain.doFilter(request, response);
   }
 
-  public void init(FilterConfig filterConfig) throws ServletException { }
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    // Not implemented
+  }
 
-  public void destroy() { }
+  @Override
+  public void destroy() {
+    // Not implemented
+  }
 
 }
